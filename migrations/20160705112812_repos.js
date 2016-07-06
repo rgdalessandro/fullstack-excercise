9b99b84dc0612fa1d0d8cc11d846ac1be77cbaa2
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-07-06 16:18:41 -0500

    all extra credit completed

--- migrations/20160705112812_repos.js
+++ migrations/20160705112812_repos.js
@@ -6,12 +6,24 @@ exports.up = function(knex, Promise) {
       table.string('repo_name');
       table.string('owner_name');
       table.integer('stargazers');
+    }),
+
+    knex.schema.createTable('contributors', function(table){
+      table.integer('id').primary();
+      table.string('contributor_name');
+    }),
+
+    knex.schema.createTable('contributions', function(table){
+      table.integer('repo_id').unsigned().references('id').inTable('repos');
+      table.integer('contributor_id').unsigned().references('id').inTable('contributors');
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-      knex.schema.dropTable('repos')
+      knex.schema.dropTable('repos'),
+      knex.schema.dropTable('contributors'),
+      knex.schema.dropTable('contributions')      
     ])
 };
--- server/index.js
+++ server/index.js
@@ -17,8 +17,9 @@ var knex = require('knex')({
 
 
 app.post('/repos/import', function (req, res) {
-  res.sendStatus(202);
   var repos = req.body.repositories;
+  var created = 0;
+  var updated = 0;
   knex.select('id').table('repos')
   .then(function(rows){
     var existingRepos = rows;
@@ -29,13 +30,57 @@ app.post('/repos/import', function (req, res) {
       var owner_name = repo.owner_name;
       var stargazers = repo.stargazers;
       if (existingRepos.indexOf(id) < 0) {
+        created++;
         knex('repos').insert({id: id, repo_name: repo_name, owner_name: owner_name, stargazers: stargazers})
-        .then(function(resp){console.log(resp)});
+        .then(function(resp){console.log("added repo", resp[0])});
       } else {
+        updated++;
         knex('repos').where({id: id}).update({repo_name: repo_name, owner_name: owner_name, stargazers: stargazers})
-        .then(function(resp){console.log(resp)});
+        .then(function(resp){console.log("updated repo", resp)});
       }
     }
+    res.status(202).send({created: created, updated: updated});
+  });
+});
+
+
+app.post('/contributors/import', function (req, res) {
+  var contributors = req.body.contributors;
+  knex.select('id').table('contributors')
+  .then(function(rows){
+    var existingContributors = rows;
+    if (existingContributors.length > 0) existingContributors = existingContributors.map(row => row.id);
+    for (contributor of contributors) {
+      var id = contributor.id;
+      var contributor_name = contributor.contributor_name;
+      if (existingContributors.indexOf(id) < 0) {
+        knex('contributors').insert({id: id, contributor_name: contributor_name})
+        .then(function(resp){console.log("added user", resp[0])});
+      } else {
+        
+      }
+    }
+    res.sendStatus(202);
+  });
+});
+
+
+app.post('/contributions/import', function (req, res) {
+  var contributions = req.body;
+  knex.select().table('contributions')
+  .then(function(rows){
+    var existingContributions = rows;
+    if (existingContributions.length > 0) existingContributions = existingContributions.map(row => [row.repo_id, row.contributor_id]);
+    for (contributor_id of contributions.contributor_ids) {
+      var repo_id = contributions.repo_id;
+      if (existingContributions.indexOf([repo_id, contributor_id]) < 0) {
+        knex('contributions').insert({repo_id: repo_id, contributor_id: contributor_id})
+        .then(function(resp){console.log("added contribution", resp)});
+      } else {
+        
+      }
+    }
+    res.sendStatus(202);
   });
 });
 
